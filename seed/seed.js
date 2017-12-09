1091e072fafdbf28b4203b8a659df82c4b01d2f9
Justin <jvcruz@ucdavis.edu>
2017-12-08 19:33:26 -0800

    seed file successful

--- seed/seed.js
+++ seed/seed.js
@@ -1,71 +1,65 @@
 // ==================================
-// DB STUFF
+// IMPORT MODULES
 // ==================================
-// TODO:
-// MIGRATE THIS TO A DB FOLDER AND FILE AFTER CONFIGURING SEED;
+
 const faker = require('faker/locale/en');
 const shortid = require('shortid');
-const Promise = require('bluebird');
-const Sequelize = require('sequelize');
+const mysql = require('promise-mysql');
 
-const db = new Sequelize({
-  database: 'Playlists',
-  username: 'root',
-  password: 'justin12',
-  dialect: 'mysql',
-});
+// ======================================
+// CREATE DATABASE/TABLES AND RESET TABLE
+// ======================================
+const dropDB = 'DROP DATABASE Playlist';
+const createDB = 'CREATE DATABASE Playlist';
+const useDB = 'USE Playlist';
 
-const Playlist = db.define('playlists', {
-  name: Sequelize.STRING,
-}, {
-  timestamps: false,
-});
+const createPlaylist = 
+  `CREATE TABLE playlists(
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    name VARCHAR(255)
+  )`;
 
-const JoinTable = db.define('userPlaylistJoins', {
-  playlistId: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-  },
-  userId: Sequelize.STRING,
-}, {
-  timestamps: false,
-});
+const createJoinTable = 
+  `CREATE TABLE joinUserPlaylist(
+    userId VARCHAR(255),
+    playlistId INT NOT NULL,
+    FOREIGN KEY(playlistId) REFERENCES playlists(id)
+  )`;
 
-JoinTable.belongsTo(Playlist);
+// ==============================
+// POPULATE PLAYLISTS
+// ==============================
+const insertPlaylist = 'INSERT INTO playlists (name) VALUES (?)';
 
-JoinTable.sync();
-Playlist.sync();
+// ==============================
+// POPULATE JOIN TABLE
+// ==============================
+const getId = '(SELECT id FROM playlists WHERE name=?)';
+const insertIntoJoin = `INSERT INTO joinUserPlaylist (userId, playlistId) VALUES (?, ${getId})`;
 
-// ==========================
-// SEED ACTION
-// ==========================
+// ==============================
+// INSTANTIATE CONNECTION
+// ==============================
+let connection;
 
-/*
- * We're going to start off with an array of promises since saving a Playlist is asynchronous
- * Let's keep in mind that Sequelize's promise suite is bluebird
- * Once we have an array of promises we use Promise.all to have an array of the payloads once
- * the promises are resolved
- * With the payload, we can now map out array and create our join tables
- *
- * Ideally since this is simply a seed file, for simplicity's sake we will use Promise.all
- * It's more efficient to just get a payload of one promise and go straight to creating the join table
- * As soon as we create our playlist
- * ^^Things to keep in mind when working on production code + live data
- */
-const playlistDataPromise = [];
+mysql.createConnection({
+  user: 'root',
+  password: 'justin',
+})
+  .then(conn => {
+    connection = conn;
+    return connection.query(dropDB);
+  })
+  .then(() => connection.query(createDB))
+  .then(() => connection.query(useDB))
+  .then(() => connection.query(createPlaylist))
+  .then(() => connection.query(createJoinTable))
+  .then(() => {
+    for (let i = 0; i < 100; i++) {
+      let name = faker.lorem.words();
+      connection.query(insertPlaylist, name)
+        .then(() => connection.query(insertIntoJoin, [shortid.generate(), name]));
+    }
 
-for (var i = 0; i < 10; i++) {
-  let name = faker.lorem.words();
-  playlistDataPromise.push(Playlist.build({ name }).save());
-}
-
-
-Promise.all(playlistDataPromise)
-  .then(data => data.map(({ dataValues }) => dataValues ))
-  .then(data => data.map(playlist => JoinTable.build({
-    playlistId: playlist.id,
-    userId: shortid.generate(),
-  }).save()))
-  .then(Promise.all)
-  .then(data => data.map(({ dataValues }) => dataValues ))
-  .then(console.log);
+    console.log('Seeding complete. Feel free to exit out with Ctrl/Command + C.');
+  });
